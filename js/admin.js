f6305f463a5dd51e3e443bee869c2de39f7c22e4
emilianolun <emilianoluna93@gmail.com>
2020-09-16 21:31:36 -0300

    Agrega destacado e imagen

--- js/admin.js
+++ js/admin.js
@@ -17,6 +17,10 @@ class GenerarFormulario {
       <textarea class="form-control"  id="Descripcion" rows="3">${descripcion}</textarea>
     </div>
     <div class="form-group">
+      <label for="exampleFormControlInput1">Imagen</label>
+      <input type="codigo" class="form-control" id="Imagen" placeholder="Imagen">
+    </div>
+    <div class="form-group">
       <label for="exampleFormControlSelect1">Categoria</label>
       <select class="form-control" id="Categoria">
         <option>Acción</option>
@@ -43,7 +47,15 @@ function subirPelicula() {
   const nombre = document.getElementById("Nombre").value;
   const descripcion = document.getElementById("Descripcion").value;
   const categoria = document.getElementById("Categoria").value;
-  const pelicula = new Peliculas(codigo, nombre, descripcion, categoria);
+  const imagen = document.getElementById("Imagen").value;
+  const pelicula = new Peliculas(
+    codigo,
+    nombre,
+    descripcion,
+    categoria,
+    false,
+    imagen
+  );
   const peliculaAAgregar = new NuevaPelicula();
 
   if (
@@ -89,11 +101,13 @@ function subirPelicula() {
 }
 
 class Peliculas {
-  constructor(Codigo, Nombre, Descripcion, Categoria) {
+  constructor(Codigo, Nombre, Descripcion, Categoria, Destacada, Imagen) {
     this._Codigo = Codigo;
     this._Nombre = Nombre;
     this._Descripcion = Descripcion;
     this._Categoria = Categoria;
+    this._Destacada = Destacada;
+    this._Imagen = Imagen;
   }
 }
 
@@ -113,9 +127,7 @@ class NuevaPelicula {
     <td><button id="${peliculas[i]._Codigo}">Borrar
   </button>
   <button id="${peliculas[i]._Nombre}">Modificar</button>
-<a><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-star" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-<path fill-rule="evenodd" d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.523-3.356c.329-.314.158-.888-.283-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767l-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288l1.847-3.658 1.846 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.564.564 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
-</svg></a>
+  <button id="${peliculas[i]._Descripcion}"> Destacar </button>
 
   </<td>
     `;
@@ -145,6 +157,20 @@ class NuevaPelicula {
           valores[5].textContent
         );
       }
+      document.getElementById(
+        peliculas[i]._Descripcion
+      ).onclick = peliculaDestacada;
+      function peliculaDestacada(e) {
+        const array = JSON.parse(localStorage.getItem("peliculas"));
+        for (var i = 0; i < array.length; i++) {
+          if (array[i]._Descripcion === e.target.id) {
+            array[i]._Destacada = true;
+          } else {
+            array[i]._Destacada = false;
+          }
+        }
+        localStorage.setItem("peliculas", JSON.stringify(array));
+      }
     }
   }
 
@@ -163,9 +189,7 @@ class NuevaPelicula {
     <td><button  id="${peliculas[i]._Codigo}">Borrar
   </button>
   <button id="${peliculas[i]._Nombre}"> Modificar</button>
-<a><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-star" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-<path fill-rule="evenodd" d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.523-3.356c.329-.314.158-.888-.283-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767l-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288l1.847-3.658 1.846 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.564.564 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
-</svg></a>
+  <button id="${peliculas[i]._Descripcion}"> Destacar </button>
   </<td>`;
     tabla.appendChild(elemento);
     document.getElementById(peliculas[i]._Codigo).onclick = deleteMovie;
@@ -192,6 +216,20 @@ class NuevaPelicula {
         valores[3].textContent,
         valores[5].textContent
       );
+    }
+    document.getElementById(
+      peliculas[i]._Descripcion
+    ).onclick = peliculaDestacada;
+    function peliculaDestacada(e) {
+      const array = JSON.parse(localStorage.getItem("peliculas"));
+      for (var i = 0; i < array.length; i++) {
+        if (array[i]._Descripcion === e.target.id) {
+          array[i]._Destacada = true;
+        } else {
+          array[i]._Destacada = false;
+        }
+      }
+      localStorage.setItem("peliculas", JSON.stringify(array));
     }
   }
 }
