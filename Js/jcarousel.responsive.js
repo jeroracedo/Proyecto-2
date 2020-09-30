f42aeedb5ca76a0228c52efb49620d2c4811e048
Valem <vale.muzzo@gmail.com>
2020-09-30 18:17:39 -0300

    Sliders arreglados con funcionamiento y todo!! =D . Tambien se agrega que se escondan los sliders por id cuando no hay peliculas en una categoria u otra.

---
+++ Js/jcarousel.responsive.js
@@ -0,0 +1,48 @@
+(function ($) {
+  $(function () {
+    var jcarousel = $(".jcarousel");
+
+    jcarousel
+      .on("jcarousel:reload jcarousel:create", function () {
+        var carousel = $(this),
+          width = carousel.innerWidth();
+
+        if (width >= 600) {
+          //si el valor del ancho es >=600
+          width = width / 6; //dividir en 6 los espacios para las imagenes
+        } else if (width >= 300) {
+          width = width / 2;
+        }
+
+        carousel.jcarousel("items").css("width", Math.ceil(width) + "px");
+      })
+      .jcarousel({
+        wrap: "circular",
+      });
+
+    $(".jcarousel-control-prev").jcarouselControl({
+      target: "-=2",
+    });
+
+    $(".jcarousel-control-next").jcarouselControl({
+      target: "+=2",
+    });
+
+    $(".jcarousel-pagination")
+      .on("jcarouselpagination:active", "a", function () {
+        $(this).addClass("active");
+      })
+      .on("jcarouselpagination:inactive", "a", function () {
+        $(this).removeClass("active");
+      })
+      .on("click", function (e) {
+        e.preventDefault();
+      })
+      .jcarouselPagination({
+        perPage: 1,
+        item: function (page) {
+          return '<a href="#' + page + '">' + page + "</a>";
+        },
+      });
+  });
+})(jQuery);
