f7fc00ae9eb102ec81a2efe2f1b144642f69986e
Valem <vale.muzzo@gmail.com>
2020-10-08 00:42:54 -0300

    iconos abm arreglados y funcionando

--- js/admin.js
+++ js/admin.js
@@ -150,12 +150,13 @@ class NuevaPelicula {
     <input type="checkbox" ${checked}
     } class="form-check-input" id="check${peliculas[i]._Codigo}">
   </div></td>
-    <td><button id="${peliculas[i]._Codigo}">Borrar
-  </button>
-  <button id="${"n" + peliculas[i]._Codigo}">Modificar</button>
-  <button id="${"d" + peliculas[i]._Codigo}"> Destacar </button>
+  <td class="d-flex flex-column align-items-center"><span id="${
+    peliculas[i]._Codigo
+  }" class="panel far fa-trash-alt"></span>
+      <span class="panel far fa-edit" id="${"n" + peliculas[i]._Codigo}"></span>
+      <span class="panel far fa-star" id="${"d" + peliculas[i]._Codigo}"></span>
 
-  </<td>
+  </td>
     `;
       tabla.appendChild(elemento);
       document.getElementById(peliculas[i]._Codigo).onclick = deleteMovie;
@@ -255,11 +256,12 @@ class NuevaPelicula {
       peliculas[i]._Codigo
     }">
   </div> </td>
-    <td><button  id="${peliculas[i]._Codigo}">Borrar
-  </button>
-  <button id="${"n" + peliculas[i]._Codigo}"> Modificar</button>
-  <button id="${"d" + peliculas[i]._Codigo}"> Destacar </button>
-  </<td>`;
+  <td class="d-flex flex-column align-items-center pt-4"><span id="${
+    peliculas[i]._Codigo
+  }" class="panel far fa-trash-alt"></span>
+      <span class="panel far fa-edit" id="${"n" + peliculas[i]._Codigo}"></span>
+      <span class="panel far fa-star" id="${"d" + peliculas[i]._Codigo}"></span>
+  </td>`;
     tabla.appendChild(elemento);
     document.getElementById(peliculas[i]._Codigo).onclick = deleteMovie;
     console.log(document.getElementById(peliculas[i]._Codigo));
