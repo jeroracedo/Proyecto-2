9f93e1067f31e780dd63ccf20d5ab7447770d83f
Valem <vale.muzzo@gmail.com>
2020-10-08 00:32:32 -0300

    subo la version que funciona de admin.js

--- js/admin.js
+++ js/admin.js
@@ -146,16 +146,16 @@ class NuevaPelicula {
     <td>${peliculas[i]._Nombre}</td>
     <td>${peliculas[i]._Descripcion}</td>
     <td>${peliculas[i]._Categoria}</td>
-    <td> 
-    <input class="mx-5 my-5" type="checkbox" ${checked}
-     class="form-check-input" id="check${peliculas[i]._Codigo}">
-  </td>
-  <td class="d-flex flex-column align-items-center pt-4"><span id="${
-    peliculas[i]._Codigo
-  }" class="panel far fa-trash-alt"></span>
-  <span class="panel far fa-edit" id="${"n" + peliculas[i]._Codigo}"></span>
-  <span class="panel far fa-star" id="${"d" + peliculas[i]._Codigo}"></span>
-  </td>
+    <td> <div class="form-group form-check" >
+    <input type="checkbox" ${checked}
+    } class="form-check-input" id="check${peliculas[i]._Codigo}">
+  </div></td>
+    <td><button id="${peliculas[i]._Codigo}">Borrar
+  </button>
+  <button id="${"n" + peliculas[i]._Codigo}">Modificar</button>
+  <button id="${"d" + peliculas[i]._Codigo}"> Destacar </button>
+
+  </<td>
     `;
       tabla.appendChild(elemento);
       document.getElementById(peliculas[i]._Codigo).onclick = deleteMovie;
